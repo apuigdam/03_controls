2fc0b95c63d77116578f8d53f0308639117e83f8
apuigdam <apuig.dam@institutcampalans.net>
2026-09-24 16:15:47 +0200

    Buto afegit

--- 03_controls/MainWindow.xaml.cs
+++ 03_controls/MainWindow.xaml.cs
@@ -23,6 +23,34 @@ namespace _03_controls
         public MainWindow()
         {
             InitializeComponent();
+            Grid elMeuGrid = new Grid();
+            this.Content = elMeuGrid;
+
+            Button btn = new Button();
+            WrapPanel elMeuPanel = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = "Hola";
+            txt1.Foreground = Brushes.Red;
+            elMeuPanel.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "Adeu";
+            txt2.Foreground = Brushes.Yellow;
+            elMeuPanel.Children.Add(txt2);
+
+            TextBlock txt3 = new TextBlock();
+            txt3.Text = "Andreu";
+            txt3.Foreground = Brushes.Green;
+            elMeuPanel.Children.Add(txt3);
+
+            btn.Width = 150;
+            btn.Height = 50;
+            btn.FontSize = 15;
+            btn.Content = elMeuPanel;
+            elMeuGrid.Children.Add(btn);
+            
+
         }
     }
 }
